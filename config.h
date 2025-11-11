845ad85c268836ef9ef411b4336f207446d50955
Kamil Koziol <koziol.kamil@protonmail.com>
2025-11-11 13:24:57 +0100

    add symbols

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=18" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=18", "Symbols Nerd Font Mono" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=18";
 static const char col_gray1[]       = "#000807";
 static const char col_gray2[]       = "#444444";
